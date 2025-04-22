a6b6b4564e1225390e3874078cffb392c03ed390
nikhilmujeeb <nikhilmujeeb@gmail.com>
2025-04-22 22:15:26 +0530

    first commit

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import PostDetail from '../pages/PostDetail';
-import EditPost from '../pages/EditPost';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/posts/:id" element={<PostDetail />} />
-      <Route path="/posts/:id/edit" element={<EditPost />} />
     </Routes>
   );
 }
